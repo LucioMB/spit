2075c1ca42be87417f9c7e07954e642767f79c8d
LucioMB <57898810+LucioMB@users.noreply.github.com>
2019-12-04 12:40:39 +0100

    Add files via upload

---
+++ src/components/food-price/partial-price.js
@@ -0,0 +1,22 @@
+import React from "react";
+import PropTypes from 'prop-types';
+
+import food from "../show/food.js";
+import Counter from "../food-price/index.js";
+
+function PartialPrice (currentPartialPrice){
+    const price = food[{price}];
+    let quantity = Counter({count});
+    for (let i = 0; i = quantity.length; i++){
+        currentPartialPrice = price * quantity;
+        return(
+            <div>{currentPartialPrice}</div>
+        );
+    }
+}
+
+PartialPrice.propTypes = {
+    currentPartialPrice: PropTypes.element.isRequired
+  };
+
+  export default PartialPrice;
